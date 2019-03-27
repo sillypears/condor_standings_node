622c5c8879cb24d7a2702d2f9d93ada817952962
sillypears <sillypears@users.noreply.github.com>
2019-03-27 10:36:38 -0400

    Added check for .env file

--- db.js
+++ db.js
@@ -1,5 +1,13 @@
 const dotenv = require('dotenv').config();
 const mysql = require('mysql');
+var fs = require('fs');
+
+fs.stat('.env', function(err, stat){
+  if(err != null && err.code == 'ENOENT') {
+    console.log("Please create .env file")
+    process.exit(1);
+  }
+});
 
 const conn = mysql.createPool({
     host    : process.env.DB_HOST,
